0bc32130f8806d398f1b018db8c1b07bbea62911
Christian G. Garcia <158063019+christiangarcia0311@users.noreply.github.com>
2025-12-14 11:38:56 +0800

    update CORS and email backend configurations

--- stream/settings.py
+++ stream/settings.py
@@ -180,24 +180,27 @@ SIMPLE_JWT = {
 
 
 # -- CORS CONFIGURATION --
-
 CORS_ALLOWED_ORIGINS = [
     "https://snsu-community-forum-app.vercel.app",
+    "https://christiangarcia.pythonanywhere.com",
     "http://localhost:8100"
 ]
+
 CORS_ALLOW_CREDENTIALS = True
 
 
 # -- EMAIL / SMTP CONFIGURATION --
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "portal.email_backends.APIEmailBackend"
 
+'''
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend" 
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS') == 'True'
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
-
+'''
 
 # --- MEDIA FILES CONFIGURATION ---
 MEDIA_URL = '/media/'
