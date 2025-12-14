c2e489f7e215a670cd0648546e26ed04c2827682
Christian G. Garcia <158063019+christiangarcia0311@users.noreply.github.com>
2025-12-14 12:47:43 +0800

    disable static media URL handling in DEBUG
    Comment out static media URL handling in DEBUG mode

--- stream/urls.py
+++ stream/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('api/v1/notifications/', include('notifications.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
